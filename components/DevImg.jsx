08b715f17e741da750d52d487ab7c8b8a788cd3c
Yongki Satria Sanjaya <yongkisatrias@gmail.com>
2024-01-04 17:10:02 +0700

    [UPDATE] Update CSS Variables

--- components/DevImg.jsx
+++ components/DevImg.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import Image from "next/image";
 
-const DevImg = () => {
-  return <div>DevImg</div>;
+const DevImg = ({ containerStyles, imgSrc }) => {
+  return (
+    <div className={`${containerStyles}`}>
+      <Image src={imgSrc} fill priority alt="photo_dev" />
+    </div>
+  );
 };
 
 export default DevImg;
--- components/Hero.jsx
+++ components/Hero.jsx
@@ -42,9 +42,43 @@ const Hero = () => {
                 <Download size={18} /> Download CV
               </Button>
             </div>
+            {/* Socials */}
+            <Socials
+              containerStyles="flex gap-x-6 mx-auto xl:mx-0"
+              IconsStyles="text-foreground text-[22px] hover:text-primary transition-all"
+            />
           </div>
           {/* Image */}
-          <div className="hidden xl:flex relative">Image</div>
+          <div className="hidden xl:flex relative">
+            {/* Badge 1 */}
+            <Badge
+              containerStyles="absolute top-[24%] -left-[5rem]"
+              icon={<RiBriefcase4Fill />}
+              endCountNum={1}
+              badgeText="Years Of Experience"
+            />
+            {/* Badge 2 */}
+            <Badge
+              containerStyles="absolute top-[80%] -left-[1rem]"
+              icon={<RiTodoFill />}
+              endCountNum={6}
+              endCountText=""
+              badgeText="Finished Projects"
+            />
+            {/* Badge 3 */}
+            <Badge
+              containerStyles="absolute top-[55%] -right-8"
+              icon={<RiTeamFill />}
+              endCountNum={6}
+              endCountText=""
+              badgeText="Happy Clients"
+            />
+            <div className="bg-hero_shape2_light dark:bg-hero_shape2_dark w-[500px] h-[500px] bg-no-repeat absolute -top-1 -right-2"></div>
+            <DevImg
+              containerStyles="bg-hero_shape w-[510px] h-[462px] bg-no-repeat relative bg-bottom"
+              imgSrc="/hero/developer.png"
+            />
+          </div>
         </div>
         {/* Icon */}
         <div className="hidden md:flex absolute left-2/4 bottom-44 xl:bottom-12 animate-bounce">
