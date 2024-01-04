7c9b45fb0dfbb63933f2d147c71b127fda4a9e1e
Yongki Satria Sanjaya <yongkisatrias@gmail.com>
2024-01-04 15:28:29 +0700

    [UPDATE] Update hero

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,10 @@
-import Image from "next/image";
+// Components
+import Hero from "@/components/Hero";
 
 export default function Home() {
-  return <main>homepage</main>;
+  return (
+    <main>
+      <Hero />
+    </main>
+  );
 }
---
+++ components/Hero.jsx
@@ -0,0 +1,58 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { Download, Send } from "lucide-react";
+
+import {
+  RiBriefcase4Fill,
+  RiTeamFill,
+  RiTodoFill,
+  RiArrowDownSLine,
+} from "react-icons/ri";
+
+// Components
+import DevImg from "./DevImg";
+import Badge from "./Badge";
+import Socials from "./Socials";
+
+const Hero = () => {
+  return (
+    <section className="py-12 xl:py-24 h-[84vh] xl:pt-28 bg-hero bg-no-repeat bg-bottom bg-cover dark:bg-none">
+      <div className="container mx-auto">
+        <div className="flex justify-between gap-x-8">
+          {/* Text */}
+          <div className="flex max-w-[600px] flex-col justify-center mx-auto xl:mx-0 text-center xl:text-left">
+            <div className="text-sm uppercase font-semibold mb-4 text-primary tracking-[4px]">
+              Fullstack Web & Mobile Developer
+            </div>
+            <h1 className="h1 mb-4">Hello, my name is Yongki Satria Sanjaya</h1>
+            <p className="subtitle max-w-[490px] mx-auto xl:mx-0">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
+              sed quam eveniet quasi nostrum numquam aperiam necessitatibus
+              reiciendis asperiores, illo officia vitae vel. Veritatis iusto
+              inventore illo animi voluptatum est!
+            </p>
+            {/* Buttons */}
+            <div className="flex flex-col gap-y-3 md:flex-row gap-x-3 mx-auto xl:mx-0 mb-12">
+              <Link href="/contact">
+                <Button className="gap-x-2">
+                  <Send size={18} /> Contact Me
+                </Button>
+              </Link>
+              <Button variant="secondary" className="gap-x-2">
+                <Download size={18} /> Download CV
+              </Button>
+            </div>
+          </div>
+          {/* Image */}
+          <div className="hidden xl:flex relative">Image</div>
+        </div>
+        {/* Icon */}
+        <div className="hidden md:flex absolute left-2/4 bottom-44 xl:bottom-12 animate-bounce">
+          <RiArrowDownSLine className="text-3xl text-primary" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
