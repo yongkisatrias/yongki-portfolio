32e434184bb7cf896f9e283cc4ea4997db2e7c31
Yongki Satria Sanjaya <yongkisatrias@gmail.com>
2024-01-04 17:24:35 +0700

    [UPDATE] Update mobile navbar

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { AlignJustify } from "lucide-react";
+
+// Components
+import Nav from "./Nav";
+import Logo from "./Logo";
+import Socials from "./Socials";
 
 const MobileNav = () => {
-  return <div>MobileNav</div>;
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <AlignJustify className="cursor-pointer" />
+      </SheetTrigger>
+      <SheetContent>
+        <div className="flex flex-col items-center justify-between h-full py-8">
+          <div className="flex flex-col items-center gap-y-32">
+            <Logo />
+            <Nav
+              containerStyles="flex flex-col items-center gap-y-6"
+              linkStyles="text-2xl"
+            />
+          </div>
+          <Socials containerStyles="flex gap-x-4" IconsStyles="text-2xl" />
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
 };
 
 export default MobileNav;
