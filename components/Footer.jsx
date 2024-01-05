481b5caeace60e7c6a3a689eefcb597d5b665876
Yongki Satria Sanjaya <yongkisatrias@gmail.com>
2024-01-05 15:38:12 +0700

    [UPDATE] Update footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,5 +1,26 @@
+import Socials from "./Socials";
+
 const Footer = () => {
-  return <footer>Footer</footer>;
+  const currentYearCopyright = new Date().getFullYear();
+
+  return (
+    <footer className="bg-secondary py-12">
+      <div className="container mx-auto">
+        <div className="flex flex-col items-center justify-between">
+          {/* Socials */}
+          <Socials
+            containerStyles="flex gap-x-6 mx-auto xl:mx-0 mb-4"
+            IconsStyles="text-primary dark:text-white/70 text-[20px] hover:text-white dark:hover:text-primary transition-all"
+          />
+          {/* Copyright */}
+          <div className="text-muted-foreground">
+            © Copyright {currentYearCopyright} Yongki Satria Sanjaya. All Rights
+            Reserved.
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
